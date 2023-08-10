e7286a294aafb360507962c7062af1085dc8c91e
Steve Regala <sregala@usc.edu>
2023-08-10 15:56:31 -0700

    Updates

--- Problem_35_SearchInsertPosition.java
+++ Problem_35_SearchInsertPosition.java
@@ -5,25 +5,29 @@ You must write an algorithm with O(log n) runtime complexity.
 */
 
 class Solution {
-   public int searchInsert(int[] nums, int target) {
-       int start = 0;
-       int end = nums.length-1;
-       int middle = 0;
-       
-       while(start <= end){
-           middle = (start+end)/2;
-           if(nums[middle] == target){
-               return middle;
-           }else if(target < nums[middle]){
-               end = middle-1;
-           }else{
-               start = middle+1;
-           }
-       }
-       
-       if(nums[middle]<target){
-           return middle+1;
-       }
-       return middle;
-   }
+    public int searchInsert(int[] nums, int target) {
+        // Binary search implementation
+        int low = 0;
+        int high = nums.length-1;
+        int mid = 0;
+
+        while(low <= high){
+            mid = (high+low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+
+        if(nums[mid]<target){
+            return mid+1;
+        }
+
+        return mid;
+    }
 }
