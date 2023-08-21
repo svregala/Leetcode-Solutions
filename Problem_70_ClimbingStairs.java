1f3cd273652e68c49b48cb88bfb3e89e04423bb9
Steve Regala <sregala@usc.edu>
2023-08-21 14:49:35 -0700

    Updates

--- Problem_70_ClimbingStairs.java
+++ Problem_70_ClimbingStairs.java
@@ -4,22 +4,23 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 */
 
 class Solution {
-   public int climbStairs(int n) {
-       int dp[] = new int[n+1];
-       /*
-       opt(i) = number of distinct ways to climb to the top
-       */
-       if(n==1){
-           return 1;
-       }else if(n==2){
-           return 2;
-       }
-       dp[0] = 0;
-       dp[1] = 1;
-       dp[2] = 2;
-       for(int i=3; i<=n; i++){
-           dp[i] = dp[i-1] + dp[i-2];
-       }
-       return dp[n];
-   }
+    public int climbStairs(int n) {
+        // Base case
+        if(n==1 || n==2){
+            return n;
+        }
+
+        // Dynamic programming - use previous values to calculate current
+        // opt(i) = number of distinct ways to climb to the top
+        int arr[] = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+
+        for(int i=3; i<=n; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+
+        return arr[n];
+    }
 }
