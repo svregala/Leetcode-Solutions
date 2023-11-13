0576a8216e3bc01da4716687aa0be0b66a16f015
Steve Regala <sregala@usc.edu>
2023-11-13 13:51:51 -0800

    Updates

--- Problem_242_ValidAnagram.java
+++ Problem_242_ValidAnagram.java
@@ -5,26 +5,14 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
 
 class Solution {
    public boolean isAnagram(String s, String t) {
-      // base case
       if(s.length() != t.length()){
          return false;
       }
-      
-      HashMap<Character, Integer> map = new HashMap<>();
-      for(char c : s.toCharArray()){
-         if(!map.containsKey(c)){
-            map.put(c, 1);
-         }else{
-            map.put(c, map.get(c)+1);
-         }
-      }
 
-      for(char c : t.toCharArray()){
-         if(!map.containsKey(c)){
-            return false;
-         }else{
-            map.put(c, map.get(c)-1);
-         }
+      Map<Character, Integer> map = new HashMap<>();
+      for(int i=0; i<s.length(); i++){
+         map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+         map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
       }
 
       for(int val : map.values()){
@@ -37,31 +25,29 @@ class Solution {
    }
 }
 
-// new solution: TC: O(n), SC:O(n) with n == length of string each string assuming they're the same length
+// TC: O(n), SC: O(1) bc of map (map or array will be at most size 26)
     // use a hash map --> put in letters and frequency count in map of string s --> iterate string t and decrement by 1 existing letters
     // final loop, if found any non zero values, then return false;
 
-// initial solution: sort strings and compare 
-    //--> TC: O(nlogn) because of sorting with n==length of string assuming they're the same length, SC:O(1)
+// TC: O(nlogn) due to sorting, SC: O(n+n)~O(n) bc of array --> O(1) map or array will be at most size 26
 /*
 public boolean isAnagram(String s, String t) {
-    // base case
     if(s.length() != t.length()){
         return false;
     }
+
     char[] s_arr = s.toCharArray();
     char[] t_arr = t.toCharArray();
 
     Arrays.sort(s_arr);
     Arrays.sort(t_arr);
 
-    String s_str = new String(s_arr);
-    String t_str = new String(t_arr);
-
-    if(s_str.equals(t_str)){
-        return true;
+    for(int i=0; i<s_arr.length; i++){
+        if(s_arr[i] != t_arr[i]){
+            return false;
+        }
     }
 
-    return false;
+    return true;
 }
 */
