9b68ecc1ea615cc4d0aa74c2c957615ea7b5405a
Steve Regala <sregala@usc.edu>
2023-11-15 19:42:42 -0800

    Updates

--- Problem_141_LinkedListCycle.java
+++ Problem_141_LinkedListCycle.java
@@ -19,30 +19,24 @@ Return true if there is a cycle in the linked list. Otherwise, return false.
 
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        // base case
-        if(head==null || head.next==null){
-            return false;
-        }
-
         ListNode slow = head;
         ListNode fast = head;
-
         while(fast!=null && fast.next!=null){
             slow = slow.next;
             fast = fast.next.next;
-            
-            if(slow == fast){
+            if(slow==fast){
                 return true;
             }
         }
-
         return false;
     }
 }
+// TC: O(n), worst case is iterating through all nodes
+// SC: O(1), created only extra space for slow and fast pointers
 
 // fast and slow pointer -- think racetrack with 2 people
     // if there exists a cycle, fast and slow are going to eventually be equal
     // think of 2 people racing on a track where one is 2x faster than the other
 // exit while loop if fast or fast.next has reached null
-    // cycle exists if fast becomes the same value as slow
+    // cycle exists if fast becomes the same value/node as slow
 // alt solution: use a hashset
--- Problem_21_MergeTwoSortedLists.java
+++ Problem_21_MergeTwoSortedLists.java
@@ -1,3 +1,9 @@
+/* 
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+Return the head of the merged linked list.
+*/
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -10,17 +16,17 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // Base case
-        if(list1 == null){
+        // base case
+        if(list1==null){
             return list2;
         }
-        else if(list2 == null){
+        else if(list2==null){
             return list1;
         }
 
-        // assign the head of merged list
+        // assign head of merged list
         ListNode head = null;
-        if(list1.val <= list2.val){
+        if(list1.val<=list2.val){
             head = list1;
             list1 = list1.next;
         }else{
@@ -29,28 +35,28 @@ class Solution {
         }
 
         ListNode curr = head;
-        while(list1 != null && list2 != null){
-            // Case 1: one val <= two val
-            if(list1.val <= list2.val){
+        while(list1!=null && list2!=null){
+            if(list1.val<=list2.val){
                 curr.next = list1;
+                curr = curr.next;
                 list1 = list1.next;
-                curr = curr.next;
-            }
-            // Case 2: one val > two val
-            else{
+            }else{
                 curr.next = list2;
-                list2 = list2.next;
                 curr = curr.next;
+                list2 = list2.next;
             }
         }
 
-        // attach the last elements
-        if(list1 == null){
+        // attach last elements
+        if(list1==null){
             curr.next = list2;
-        }else if(list2 == null){
+        }else{
             curr.next = list1;
         }
 
         return head;
     }
 }
+// TC: O(n) with n = total nodes in both list1 and list 2, 
+    // worst case is iterating through each node in both list1 and list 2
+// SC: O(1) because created only 2 extra listnodes
