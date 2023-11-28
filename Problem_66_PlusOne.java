69f61d4f0d27588fdf0b40794a973c6c11dc8470
Steve Regala <sregala@usc.edu>
2023-11-27 23:56:36 -0800

    Updates

--- Problem_66_PlusOne.java
+++ Problem_66_PlusOne.java
@@ -7,7 +7,6 @@ Increment the large integer by one and return the resulting array of digits.
 
 class Solution {
    public int[] plusOne(int[] digits) {
-
       // start from the end of array, if digit is >9, convert it to 0
       // if digits is <9, add 1 to it, then return digits
       for(int i=digits.length-1; i>=0; i--){
@@ -19,10 +18,13 @@ class Solution {
       }
 
       // after turning numbers to 0, add 1 to beginning of array
-      // at this point, converted everything to 0, so we have to add a 1 at the beginning
+      // IMPORTANT: at this point, converted everything to 0, so we have to add a 1 at the beginning
       digits = new int[digits.length+1];
       digits[0] = 1;
-
+   
       return digits;
    }
 }
+
+// TC: O(n), worst case is iterating through whole array and turning all digits to 0
+// SC: O(n), worst case is reaching the end of for loop (since everything was converted to 0) and then creating new array to return
