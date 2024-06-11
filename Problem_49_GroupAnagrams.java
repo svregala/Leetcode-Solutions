f19c910985736914dbc4af5b22b5c055742efca6
Steve Regala <svregala@gmail.com>
2024-06-11 15:48:53 -0700

    Update Problem_49_GroupAnagrams.java

--- Problem_49_GroupAnagrams.java
+++ Problem_49_GroupAnagrams.java
@@ -4,24 +4,25 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
  */
 
 class Solution {
-public List<List<String>> groupAnagrams(String[] strs) {
+   public List<List<String>> groupAnagrams(String[] strs) {
+      Map<String, ArrayList<String>> m = new HashMap<>();
 
-   Map<String, ArrayList<String>> m = new HashMap<>();
-   for(String s : strs){
-      char[] str = s.toCharArray();
-      Arrays.sort(str);
-      String key = new String(str);
-
-      if(m.containsKey(key)){
-         m.get(key).add(s);
-      }else{
-         ArrayList<String> val = new ArrayList<>(Arrays.asList(s));
-         m.put(key, val);
+      for(String s : strs){
+         char[] sArr = s.toCharArray();
+         Arrays.sort(sArr);
+         String key = new String(sArr);
+         if(m.containsKey(key)){
+            m.get(key).add(s);
+         }
+         else{
+            ArrayList<String> val = new ArrayList<>();
+            val.add(s);
+            m.put(key, val);
+         }
       }
-   }
 
-   return new ArrayList<List<String>>(m.values());
-}
+      return new ArrayList<List<String>>(m.values());
+  }
 }
 
 // TC: O(n*mlogm) with n=length of string list, and m=length of longest string in list
